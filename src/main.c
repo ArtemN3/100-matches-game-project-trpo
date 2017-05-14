21e6b414d20d75001878eba78fb94d87c6314f4f
NikMargo <Ritosik25@yandex.ru>
2017-05-15 02:06:10 +0700

    Creature directory src

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <stdbool.h>
+#include <locale.h>
+#include <progect.h>
+
+int main()
+{
+setlocale(LC_ALL,"RUS");
+int matches = 100;
+int  input,x; 
+bool isWon = false;
+printf("Выберите порядок хода 1 или 2\n");
+scanf("%i", &x);
+    while (1) { 
+        if (x==1 && matches != 0){ 
+            matches = player_turn(&input,matches,&isWon);
+	    printf("\nОсталось %d спичек.\n", matches);
+            x = 2;
+        } else if (x==2 && matches != 0) {
+            matches = Comp_turn(input,matches,&isWon);
+	    printf("\nОсталось %d спичек. \n", matches);
+            x = 1;
+        }
+        if (matches == 0) {
+            break;
+        } 
+    } 
+    if (isWon) printf("\nПобедил игрок!\n"); 
+    else printf("\nПобедил компьютер!\n");
+}
