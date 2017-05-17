bfa250f86a772ef3cd188b4bb806bcd774cbf774
Артём <arteym1234@mail.ru>
2017-05-17 22:01:55 +0700

    Correction of failed tests

--- test/validation_test.c
+++ test/validation_test.c
@@ -3,11 +3,11 @@
 
 CTEST(Getinput,input0matches0correct0){
 	const int result = getinput(0,0,0);
-	const int expected = 1;
+	const int expected = 0;
 	ASSERT_EQUAL(expected,result);
 }
 CTEST(Getinput,input2matches0correct1){
-	const int result = getinput(2,0,1);
-	const int expected = 0;
+	const int result = getinput(2,5,1);
+	const int expected = 1;
 	ASSERT_EQUAL(expected,result);
 }
