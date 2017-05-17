80e44b3f367bc3f6c14f59a3fa82a884d80965b1
Артём <arteym1234@mail.ru>
2017-05-17 20:22:55 +0700

    Test for Player and Comp turn

--- test/progect_test.c
+++ test/progect_test.c
@@ -1,8 +1,23 @@
-#include "progect.h"
+#include "func.h"
 #include "ctest.h"
 
-CTEST(ProgTest,plyer){
+CTEST(PlayerTurn,3inputs90matches){
 	const int result = player_turn(3,90);
 	const int expected = 87;
 	ASSERT_EQUAL(expected, result);
-}	
+}
+CTEST(PlayerTurn,9inputs5matches){
+	const int result = player_turn(9,5);
+	const int expected = -1;
+	ASSERT_EQUAL(expected, result);
+}
+CTEST(CompTurn,15inputs5matches){
+	const int result = Comp_turn(15,5);
+	const int expected = -1;
+	ASSERT_EQUAL(expected, result);
+}
+CTEST(CompTurn,5inputs5matches){
+	const int result = Comp_turn(5,5);
+	const int expected = 0;
+	ASSERT_EQUAL(expected, result);
+}
