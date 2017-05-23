e0986dc544b0bab863cd5f978a6338c29a34fc3f
NikMargo <Ritosik25@yandex.ru>
2017-05-24 02:37:21 +0700

    Fixed due to function changes

--- src/main.c
+++ src/main.c
@@ -8,18 +8,30 @@ int main()
 {
 setlocale(LC_ALL,"RUS");
 int matches = 100;
-int  input,x; 
+int input,x; 
 bool isWon = false;
+int correct;
 printf("Выберите порядок хода 1 или 2\n");
 scanf("%i", &x);
     while (1) { 
         if (x==1 && matches != 0){ 
-            matches = player_turn(&input,matches,&isWon);
-	    printf("\nОсталось %d спичек.\n", matches);
+            while(1){
+                input = vvod(input);
+                correct = getinput(input, matches,correct);
+                if (correct == 1) {
+                    break;
+                } else 
+                printf("Неверно! Повторите ввод\n");
+            }
+            matches = player_turn(input,matches,correct,&isWon);
+            printf("\nОсталось %d спичек.\n",matches);
             x = 2;
         } else if (x==2 && matches != 0) {
-            matches = Comp_turn(input,matches,&isWon);
-	    printf("\nОсталось %d спичек. \n", matches);
+            printf("Ход компьютера.\n");
+            input = rand()%10+1;
+            matches = Comp_turn(&input,matches,&isWon);
+            printf("Компьютер взял %d спичек\n",input);
+            printf("\nОсталось %d спичек. \n", matches);
             x = 1;
         }
         if (matches == 0) {
