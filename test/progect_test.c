62cde96a9f6cffac653cecaef2cd8c9a9da76e13
NikMargo <Ritosik25@yandex.ru>
2017-05-26 23:38:58 +0700

    Test Optimization

--- test/progect_test.c
+++ test/progect_test.c
@@ -14,26 +14,18 @@ CTEST(PlayerTurn,9inputs5matches){
 	ASSERT_EQUAL(expected, result);
 }
 
-CTEST(CompTurn,15inputs5matches){
-	int input;
-	input = 6;
-	const int result = Comp_turn(&input,6);
-	const int expected = 0;
+CTEST(CompTurn,5inputs60)
+{
+	int input = 5;
+	const int result = Comp_turn(&input,60);
+	const int expected = 55;
 	ASSERT_EQUAL(expected, result);
 }
 
-CTEST(CompTurn,5inputs6matches){
-	int input;
-	input = 5;
-	const int result = Comp_turn(&input,6);
-	const int expected = 1;
-	ASSERT_EQUAL(expected, result);
-}
-
-CTEST(CompTurn,10inputs6matches){
-	int input;
-	input = 10;
-	const int result = Comp_turn(&input,6);
-	const int expected = 0;
+CTEST(CompTurn,10inputs100matches)
+{
+    int input =10;
+	const int result = Comp_turn(&input,100);
+	const int expected = 90;
 	ASSERT_EQUAL(expected, result);
 }
