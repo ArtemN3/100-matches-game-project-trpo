437770aada66afaa15b7479e2bdfdb98a679c79c
Артём <arteym1234@mail.ru>
2017-05-28 22:32:57 +0700

    Updated tests

--- test/progect_test.c
+++ test/progect_test.c
@@ -15,15 +15,31 @@ CTEST(PlayerTurn,9inputs5matches)
 	const int expected = -1;
 	ASSERT_EQUAL(expected, result);
 }
-CTEST(CompTurn,15inputs5matches)
+CTEST(PlayerTurn,9inputs9matches)
 {
-	const int result = Comp_turn(15,5);
+	const int result = player_turn(9,9);
 	const int expected = -1;
+	ASSERT_EQUAL(expected, result);
+}
+CTEST(CompTurn,10inputs5matches)
+{
+	int input = 10;
+	const int result = Comp_turn(&input,25);
+	const int expected = 15;
 	ASSERT_EQUAL(expected,result);
 }
-CTEST(CompTurn,5inputs6matches)
+
+CTEST(CompTurn,0inputs10matches)
 {
-	const int result = Comp_turn(5,6);
-	const int expected = 1;
-	ASSERT_EQUAL(expected, result);
+	int input = 0;
+	const int result = Comp_turn(&input,10);
+	const int expected = 10;
+	ASSERT_EQUAL(expected,result);
+}
+
+CTEST(CompTurn, 100inputs100matches)
+{
+	const int result = player_turn(100,100,0);
+	const int expected = -1;
+	ASSERT_EQUAL(expected,result);
 }
