e872f306bae406473ce8986bcbafaadefc8d1222
Артём <arteym1234@mail.ru>
2017-05-28 20:45:37 +0700

    Tuning test

--- test/progect_test.c
+++ test/progect_test.c
@@ -2,30 +2,28 @@
 #include "ctest.h"
 #include <stdio.h>
 
-CTEST(PlayerTurn,3inputs90matches){
+CTEST(PlayerTurn,3inputs90matches)
+{
 	const int result = player_turn(3,90,1);
 	const int expected = 87;
 	ASSERT_EQUAL(expected, result);
 }
 
-CTEST(PlayerTurn,9inputs5matches){
+CTEST(PlayerTurn,9inputs5matches)
+{
 	const int result = player_turn(9,5);
 	const int expected = -1;
 	ASSERT_EQUAL(expected, result);
 }
-
-CTEST(CompTurn,5inputs60)
+CTEST(CompTurn,15inputs5matches)
 {
-	int input = 5;
-	const int result = Comp_turn(&input,60);
-	const int expected = 55;
-	ASSERT_EQUAL(expected, result);
+	const int result = Comp_turn(15,5);
+	const int expected = -1;
+	ASSERT_EQUAL(expected,result);
 }
-
-CTEST(CompTurn,10inputs100matches)
+CTEST(CompTurn,5inputs6matches)
 {
-    int input =10;
-	const int result = Comp_turn(&input,100);
-	const int expected = 90;
+	const int result = Comp_turn(5,6);
+	const int expected = 1;
 	ASSERT_EQUAL(expected, result);
 }
